8a48cb280e814d5fd57f1434a504f7c5c3ed7208
Smitesh Tamboli <smitesh.t@gmail.com>
2024-04-14 18:45:39 +0530

    Updated file

--- best-time-to-buy-sell-stock-121/best_time_to_buy_sell_stock_121.cpp
+++ best-time-to-buy-sell-stock-121/best_time_to_buy_sell_stock_121.cpp
@@ -3,24 +3,33 @@
 
 using namespace std;
 
-static void buy_sell_stock_max_profit_121()
-{
-    vector<int> prices = { 2,4,1 };
-    int buying_price = prices[0];
+int maxProfit(vector<int>& prices) {
+
+    int buy = prices[0]; // first Buy
     int profit = 0;
 
-    for (int ind = 0; ind < prices.size(); ind++)
+    for (int i = 1; i < prices.size(); i++)
     {
-        if (prices[ind] < buying_price) {
-            buying_price = prices[ind];
+        // if Buy > next price => Buy = new_price 
+        if (buy > prices[i])
+        {
+            buy = prices[i];
         }
         else
         {
-            int current_profit = prices[ind] - buying_price;
-            profit = current_profit > profit ? current_profit : profit;
+            // get max profit
+            profit = max(profit, prices[i] - buy);
         }
     }
 
-    cout << profit << endl;
-   
+    return profit;
+
+}
+
+
+int main()
+{
+    vector<int> prices = { 7,6,4,3,1 };
+
+    cout<< maxProfit(prices);
 }
